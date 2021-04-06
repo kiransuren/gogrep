371e81dfdbd273a17ca806a30ac34854bc01f608
kiransuren <kirank.suren@gmail.com>
2021-04-06 01:54:00 -0400

    prettify dev.sh

--- dev.sh
+++ dev.sh
@@ -1,2 +1,13 @@
+# Development build and run script
+
+echo -e "-------------------------- \n"
+
+# Build project to ./build/gogrep.exe
+echo "1/2: Building project to ./build/gogrep.exe"
 go build -o ./build/gogrep.exe ./cmd/gogrep.go
+
+# Running ./build/gogrep.exe
+echo -e "2/2: Running ./build/gogrep.exe \n"
+echo -e "-------------------------- \n"
+
 ./build/gogrep.exe
